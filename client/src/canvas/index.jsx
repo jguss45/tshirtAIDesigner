1c6f497e5c730fd7298f499368e04ea668ef3891
Joe <jguss45@gmail.com>
2023-05-06 21:00:01 +0200

    customizer backdrop

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,19 +1,27 @@
 import { Canvas } from '@react-three/fiber';
 import { Environment, Center } from '@react-three/drei';
+import { useSnapshot } from 'valtio';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
 import CameraRig from './CameraRig';
-
+import state from '../store';
 
 const CanvasModel = () => {
+  const snap = useSnapshot(state)
+
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={snap.intro ? { position: [0,0,0], fov: 50 } : { position: [0,0,0], fov: 25 }}
+      gl={{preserveDrawingBuffer: true }}
+      className='w-full max-w-full h-full transition-all ease-in'
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
        <CameraRig>
-        {/* <Backdrop />  */}
+        <Backdrop />
         <Center>
           <Shirt />
         </Center>
